2261fbe0df6a8f22feee2049c188dd30785b604a
Soumyakant07 <61344848+Soumyakant07@users.noreply.github.com>
2022-01-24 18:44:07 +0530

    Add files via upload
    access modifiers

---
+++ src/package1/Access1.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Access1 {
+	
+	//default, public, private and protected
+	
+	public int hours = 3;
+	public int minutes = 47;
+	
+	int getHours() {
+		return hours;
+	}
+
+}
